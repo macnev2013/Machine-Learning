c8c0d9fdb88319b6b5e59f62f744d7d991723fb4
Nevil Macwan <root@ip-172-31-25-117.ec2.internal>
2019-07-06 14:18:59 +0000

    Retry added

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ p = subprocess.Popen("systemctl status mysql", stdout = subprocess.PIPE, shell=T
 (output, err) = p.communicate()
 p.status = p.wait()
 ans = "running" in output
+if(ans == False):
+	print "MySql is down :("
+	print "Retrying"
+	p = subprocess.Popen("systemctl restart mysql", stdout = subprocess.PIPE, shell=True)
+	(output, err) = p.communicate()
 print "output :" , ans
 
